32c846fbe117dc67319df1129b202715d6268a11
Bryce Milton - MSFT <brycem@live.com>
2017-01-23 12:05:56 -0800

    Integrating deltas from upstream/master

--- build.psm1
+++ build.psm1
@@ -37,15 +37,6 @@ if ($IsLinux) {
     $IsUbuntu14 = $IsUbuntu -and $LinuxInfo.VERSION_ID -match '14.04'
     $IsUbuntu16 = $IsUbuntu -and $LinuxInfo.VERSION_ID -match '16.04'
     $IsCentOS = $LinuxInfo.ID -match 'centos' -and $LinuxInfo.VERSION_ID -match '7'
-    $IsFedora = $LinuxInfo.ID -match 'fedora' -and $LinuxInfo.VERSION_ID -ge 24
-    $IsRedHatFamily = $IsCentOS -or $IsFedora
-
-    # Workaround for temporary LD_LIBRARY_PATH hack for Fedora 24
-    # https://github.com/PowerShell/PowerShell/issues/2511
-    if ($IsFedora -and (Test-Path ENV:\LD_LIBRARY_PATH)) {
-        Remove-Item -Force ENV:\LD_LIBRARY_PATH
-        Get-ChildItem ENV:
-    }
 }
 
 #
@@ -90,7 +81,6 @@ function Start-PSBuild {
                      "ubuntu.16.04-x64",
                      "debian.8-x64",
                      "centos.7-x64",
-                     "fedora.24-x64",
                      "win7-x64",
                      "win7-x86",
                      "win81-x64",
@@ -451,7 +441,6 @@ function New-PSOptions {
                      "ubuntu.16.04-x64",
                      "debian.8-x64",
                      "centos.7-x64",
-                     "fedora.24-x64",
                      "win7-x86",
                      "win7-x64",
                      "win81-x64",
@@ -964,15 +953,6 @@ function Install-Dotnet {
     }
 }
 
-function Get-RedHatPackageManager {
-    if ($IsRedHatFamily -and $IsCentOS) {
-        "yum"
-    } elseif ($IsRedHatFamily -and $IsFedora) {
-        "dnf"
-    } else {
-        throw "Error determining package manager for this distribution."
-    }
-}
 
 function Start-PSBootstrap {
     [CmdletBinding(
@@ -1029,7 +1009,7 @@ function Start-PSBootstrap {
                 Invoke-Expression "$sudo apt-get update"
                 Invoke-Expression "$sudo apt-get install -y -qq $Deps"
             }
-        } elseif ($IsRedHatFamily) {
+        } elseif ($IsCentOS) {
             # Build tools
             $Deps += "which", "curl", "gcc-c++", "cmake", "make"
 
@@ -1039,11 +1019,9 @@ function Start-PSBootstrap {
             # Packaging tools
             if ($Package) { $Deps += "ruby-devel", "rpm-build", "groff" }
 
-            $PackageManager = Get-RedHatPackageManager
-
             # Install dependencies
             Start-NativeExecution {
-                Invoke-Expression "$sudo $PackageManager install -y -q $Deps"
+                Invoke-Expression "$sudo yum install -y -q $Deps"
             }
         } elseif ($IsOSX) {
             precheck 'brew' "Bootstrap dependency 'brew' not found, must install Homebrew! See http://brew.sh/"
@@ -1223,7 +1201,7 @@ function Start-PSPackage {
         [ValidatePattern("^powershell")]
         [string]$Name = "powershell",
 
-        # Ubuntu, CentOS, Fedora, OS X, and Windows packages are supported
+        # Ubuntu, CentOS, and OS X, and Windows packages are supported
         [ValidateSet("deb", "osxpkg", "rpm", "msi", "appx", "zip")]
         [string[]]$Type,
 
@@ -1277,7 +1255,7 @@ function Start-PSPackage {
         $Type = if ($IsLinux) {
             if ($LinuxInfo.ID -match "ubuntu") {
                 "deb"
-            } elseif ($IsRedHatFamily) {
+            } elseif ($LinuxInfo.ID -match "centos") {
                 "rpm"
             } else {
                 throw "Building packages for $($LinuxInfo.PRETTY_NAME) is unsupported!"
@@ -1388,8 +1366,8 @@ function New-UnixPackage {
                 }
         }
         "rpm" {
-            if (!$IsRedHatFamily) {
-                throw ($ErrorMessage -f "Redhat Family")
+            if (!$IsCentOS) {
+                throw ($ErrorMessage -f "CentOS")
             }
         }
         "osxpkg" {
@@ -1453,7 +1431,7 @@ It consists of a cross-platform command-line shell and associated scripting lang
 
     New-Item -Force -ItemType SymbolicLink -Path "/tmp/$Name" -Target "$Destination/$Name" >$null
 
-    if ($IsRedHatFamily) {
+    if ($IsCentos) {
         # add two symbolic links to system shared libraries that libmi.so is dependent on to handle
         # platform specific changes. This is the only set of platforms needed for this currently
         # as Ubuntu has these specific library files in the platform and OSX builds for itself 
@@ -1577,7 +1555,7 @@ esac
         } elseif ($IsUbuntu16) {
             $Dependencies += "libicu55"
         }
-    } elseif ($IsRedHatFamily) {
+    } elseif ($IsCentOS) {
         $Dependencies = @(
             "glibc",
             "libcurl",
@@ -1600,34 +1578,26 @@ esac
         $Iteration += "ubuntu1.16.04.1"
     }
 
-    # We currently only support CentOS 7 and Fedora 24+
+    # We currently only support CentOS 7
     # https://fedoraproject.org/wiki/Packaging:DistTag
-    if ($IsCentOS) {
-        $rpm_dist = "el7.centos"
-    } elseif ($IsFedora) {
-        $version_id = $LinuxInfo.VERSION_ID
-        $rpm_dist = "fedora.$version_id"
-    }
-
+    $rpm_dist = "el7.centos"
 
     $Arguments = @(
         "--force", "--verbose",
         "--name", $Name,
         "--version", $Version,
         "--iteration", $Iteration,
+        "--rpm-dist", $rpm_dist,
         "--maintainer", "PowerShell Team <PowerShellTeam@hotmail.com>",
         "--vendor", "Microsoft Corporation",
         "--url", "https://microsoft.com/powershell",
         "--license", "MIT License",
         "--description", $Description,
         "--category", "shells",
+        "--rpm-os", "linux",
         "-t", $Type,
         "-s", "dir"
     )
-    if ($IsRedHatFamily) {
-        $Arguments += @("--rpm-dist", $rpm_dist)
-        $Arguments += @("--rpm-os", "linux")
-    }
     foreach ($Dependency in $Dependencies) {
         $Arguments += @("--depends", $Dependency)
     }
@@ -2616,7 +2586,6 @@ function Start-CrossGen {
                      "ubuntu.16.04-x64",
                      "debian.8-x64",
                      "centos.7-x64",
-                     "fedora.24-x64",
                      "win7-x86",
                      "win7-x64",
                      "win81-x64",
@@ -2683,8 +2652,6 @@ function Start-CrossGen {
             "ubuntu.14.04-x64"
         } elseif ($IsCentOS) {
             "rhel.7-x64"
-        } elseif ($IsFedora) {
-            "fedora.24-x64"
         }
     } elseif ($IsOSX) {
         "osx.10.10-x64"
